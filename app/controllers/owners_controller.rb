3b70e52999caa8513261258e57cdb9eed03a12b1
Thomas Pope <tpope0928@gmail.com>
2019-03-04 15:29:43 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class OwnersController < ApplicationController
 
   get '/owners' do
@@ -7,15 +6,13 @@ class OwnersController < ApplicationController
   end
 
   get '/owners/new' do
-    #In order to dynamically generate these checkboxes,
-    #we need to load up all of the pets from the database.
     @pets = Pet.all
     erb :'/owners/new'
   end
 
   post '/owners' do
     @owner = Owner.create(params[:owner])
-    if !params["pet"]["name"].empty? #if statement to check whether or not the value of params["pet"]["name"] is an empty string.
+    if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
     redirect "owners/#{@owner.id}"
@@ -32,17 +29,6 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do
-    ##### bug fix
-    if !params[:owner].keys.include?("pet_ids")
-      params[:owner]["pet_ids"] = []
-    end
-    #####
 
-    @owner = Owner.find(params[:id])
-    @owner.update(params["owner"])
-    if !params["pet"]["name"].empty?
-      @owner.pets << Pet.create(name: params["pet"]["name"])
-    end
-    redirect "owners/#{@owner.id}"
   end
 end
